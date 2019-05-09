4732d42b585d472ab7c3fcdcad70febd4c421bad
Mike <mikeritch@hotmail.com>
2019-05-09 19:32:37 +0100

    materialize.css
    Demo Draft

---
+++ js/bin/init.js
@@ -0,0 +1,10 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {responsiveThreshold: 0});
+		var instances = 															 
+  });*/
+
+
+/*document.onload = function () {*/
+M.AutoInit();
+ /* };*/
